6aef199f427e4393cabd1d6f30108963fca4fcd8
Mika <42176090+zhady5@users.noreply.github.com>
2024-12-30 02:26:37 +0500

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,9 @@ body_style = """
     <style> 
       body { 
         max-width: 1200px; /* Максимальная ширина */ 
-        margin-left: auto; 
-        margin-right: auto;
+        #margin-left: auto; 
+        #margin-right: auto;
+        backgroundColor: #ffb347,
         padding: 300px; /* Отступы слева и справа */ }
     </style> """
 
