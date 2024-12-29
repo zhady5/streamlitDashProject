bd4c39ccfd9174bb00ba24b1dcd202d488dbcf53
Mika <42176090+zhady5@users.noreply.github.com>
2024-12-30 01:21:54 +0500

    Add files via upload

---
+++ fig_heatmap.py
@@ -0,0 +1,93 @@
+import pandas as pd
+import plotly
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+import streamlit as st
+from functions import get_current_previous_sums, date_ago
+import datetime
+
+def create_heatmap(filtered_df):
+    
+    # Генерация данных
+    filtered_df = filtered_df[['date', 'hour', 'cnt']].rename(columns={'cnt': 'publications'}).sort_values('date')
+    raw_index = filtered_df.set_index(['date', 'hour'])
+    
+    dates = pd.to_datetime(filtered_df.date).unique().tolist()
+    index = pd.MultiIndex.from_product([filtered_df.date.unique(), range(1, 25)], names=['date', 'hour'])
+    raw = pd.DataFrame(index=index)
+    df = raw.merge(raw_index, left_index=True, right_index=True, how='left')
+    df.fillna(0, inplace=True)
+    df = df.reset_index().drop_duplicates(subset=['date', 'hour']).set_index(['date', 'hour'])
+    
+    # Преобразование данных в формат, подходящий для heatmap
+    z_values = df['publications'].unstack(level=-1)
+    x_labels = [str(hour) for hour in range(1, 25)]
+    y_labels = [date.strftime('%Y-%m-%d') for date in dates]
+    
+    fig = go.Figure(
+        data=[
+            go.Heatmap(
+                z=pd.DataFrame([[1] * len(x_labels)] * len(y_labels), columns=range(1, 25), index=y_labels),
+                x=x_labels,
+                y=y_labels,
+                colorscale=[[0, '#ffb347'], [1, '#ffb347']],
+                showscale=False,
+                hovertemplate='%{y} <br>%{x} ч <br>Публикаций: %{z}<extra></extra>'
+            ),
+            go.Heatmap(
+                z=z_values,
+                x=x_labels,
+                y=y_labels,
+                colorscale=[[0, '#F5DEB3'], [1, "#006a4e"]],
+                showscale=False,
+                xgap=10,
+                ygap=10,
+                hovertemplate='%{y} <br>%{x} ч <br>Публикаций: %{z}<extra></extra>'
+            )
+        ]
+    ).update_layout(
+        font_family='Arial',
+        margin=dict(l=30, r=50, t=50, b=20),
+        paper_bgcolor='#ffb347',
+        plot_bgcolor='#ffb347',
+        legend_title_font_color="#212121",
+        legend_font_color="#212121",
+        legend_borderwidth=0,
+        hoverlabel_font_family='Arial',
+        hoverlabel_font_size=12,
+        hoverlabel_font_color='#212121',
+        hoverlabel_align='auto',
+        hoverlabel_namelength=-1,
+        hoverlabel_bgcolor='#FAFAFA',
+        hoverlabel_bordercolor='#E5E4E2'
+    )
+    
+    # Ограничиваем количество меток на оси Y до 10
+    if len(y_labels) > 10:
+        y_labels_subset = y_labels[::max(len(y_labels) // 10, 1)]
+    else:
+        y_labels_subset = y_labels
+    
+    # Перемещение подписей часов наверх
+    fig.update_xaxes(side="top", tickfont=dict(family='Arial', size=12), title_font=dict(family='Arial', size=14))
+    
+    fig.update_yaxes(
+        autorange="reversed",
+        dtick=max(len(y_labels) // 10, 1),
+        ticktext=y_labels,
+        tickvals=[datetime.strptime(date, "%Y-%m-%d").timestamp() for date in y_labels_subset],
+        tickformat="%b %d, %y",
+        tickfont={"family": "Arial", "size": 8},
+        title_font={"family": "Arial", "size": 14}
+    )
+    
+    # Добавляем полосу прокрутки для оси Y
+    fig.update_layout(
+        font_size=9,
+        yaxis_title="Дата",
+        xaxis_title="Часы",
+        yaxis=dict(autorange="reversed")  # tickangle=45,  # Наклон меток для улучшения читаемости
+    )
+
+    return fig
