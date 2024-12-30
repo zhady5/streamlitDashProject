92ac85c57b7b1b25a6011a394d4f7fa230bff0ad
Mika <42176090+zhady5@users.noreply.github.com>
2024-12-30 19:28:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -58,40 +58,40 @@ def main():
     st.set_page_config(layout="centered" )
     st.markdown(
       """
-  <style>
-  .reportview-container .markdown-text-container {
-      font-family: monospace;
-  }
-  .sidebar .sidebar-content {
-      background-image: linear-gradient(#2e7bcf,#2e7bcf);
-      color: white;
-  }
-  .Widget>label {
-      color: white;
-      font-family: monospace;
-  }
-  [class^="st-b"]  {
-      color: white;
-      font-family: monospace;
-  }
-  .st-bb {
-      background-color: transparent;
-  }
-  .st-at {
-      background-color: #0c0080;
-  }
-  footer {
-      font-family: monospace;
-  }
-  .reportview-container .main footer, .reportview-container .main footer a {
-      color: #0c0080;
-  }
-  header .decoration {
-      background-image: none;
-  }
-  
-  </style>
-  """,
+                <style>
+                .reportview-container .markdown-text-container {
+                    font-family: monospace;
+                }
+                .sidebar .sidebar-content {
+                    background-image: linear-gradient(#2e7bcf,#2e7bcf);
+                    color: white;
+                }
+                .Widget>label {
+                    color: white;
+                    font-family: monospace;
+                }
+                [class^="st-b"]  {
+                    color: white;
+                    font-family: monospace;
+                }
+                .st-bb {
+                    background-color: #ffb347;
+                }
+                .st-at {
+                    background-color: #ffb347;
+                }
+                footer {
+                    font-family: monospace;
+                }
+                .reportview-container .main footer, .reportview-container .main footer a {
+                    color: #ffb347;
+                }
+                header .decoration {
+                    background-image: none;
+                }
+                
+                </style>
+                """,
       unsafe_allow_html=True,
   )
 
