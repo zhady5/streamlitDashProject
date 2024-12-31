e7953dadf96db24519d7a0ae622c7014e1997b50
Mika <42176090+zhady5@users.noreply.github.com>
2024-12-31 18:20:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -105,8 +105,8 @@ def main():
     col1, col2 = st.columns(2)
     with col1:    
         st.plotly_chart(fig_posts, use_container_width=True)
-        slider = create_slider(subs, selected_channel)
-        fig_subs_pos_neg = create_subs_pos_neg(subs, selected_channel, slider)
+        #slider = create_slider(subs, selected_channel)
+        fig_subs_pos_neg = create_subs_pos_neg(subs, selected_channel, create_slider(subs, selected_channel))
         st.plotly_chart(fig_subs_pos_neg, use_container_width=True)
     with col2:
         st.plotly_chart(fig_subs, use_container_width=True)
