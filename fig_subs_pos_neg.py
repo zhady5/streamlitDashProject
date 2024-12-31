de1b142fbecfcc9f52782e6f3914f9698d68435d
Mika <42176090+zhady5@users.noreply.github.com>
2024-12-31 17:30:35 +0500

    Update fig_subs_pos_neg.py

--- fig_subs_pos_neg.py
+++ fig_subs_pos_neg.py
@@ -73,12 +73,12 @@ def create_slider(subs, channel):
     step = 86400 # секунд в одни сутки
     
     # Определяем начальные значения слайдера
-    initial_value = [0, time_delta]
+    initial_value = [int(0), int(time_delta)]
     
     # Отображаем слайдер
     return st.slider(
                         'Выберите диапазон дат:',
-                        min_value=0,
+                        min_value=int(0),
                         max_value=int(time_delta),
                         value=initial_value,
                         step=int(step)
