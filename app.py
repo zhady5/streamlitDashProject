d59538668e2609ea70cdae62bcfd7f7521b902b0
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-02 19:47:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -201,7 +201,8 @@ def main():
             return ['background-color: lightgreen' if v else '' for v in is_large]
         
         styled_df = df.style.apply(highlight_percentages, subset=['Percent New Views'])
-        st.dataframe(styled_df[columns_to_show])
+        #st.dataframe(styled_df[columns_to_show])
+        st.dataframe(styled_df.to_html(), unsafe_allow_html=True)
 
 
 
