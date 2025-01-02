cb84790fa842ccad5e8bc2c095a87ecd3f6904b7
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-02 17:24:34 +0500

    Update fig_subs_pos_neg.py

--- fig_subs_pos_neg.py
+++ fig_subs_pos_neg.py
@@ -3,9 +3,8 @@ import pandas as pd
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
 
-
-def create_subs_pos_neg(subs, channel, slider_range): #, slider_range
-    if channel is None or slider_range is None:
+def create_subs_pos_neg(subs, channel, date_range):
+    if channel is None or date_range is None:
         st.write({})  # Вывод пустой фигуры
         return
 
@@ -18,21 +17,34 @@ def create_subs_pos_neg(subs, channel, slider_range): #, slider_range
 
     # Преобразуем строку в datetime
     subdf_channel.loc[:, 'datetime'] = pd.to_datetime(subdf_channel['datetime'])
-    start_time = subdf_channel['datetime'].min() + pd.Timedelta(seconds=slider_range[0])
-    end_time = subdf_channel['datetime'].min() + pd.Timedelta(seconds=slider_range[1])
+    start_time, end_time = date_range
 
-    filtered_df = subdf_channel[(subdf_channel['datetime'] >= start_time) & (subdf_channel['datetime'] <= end_time)]
+    filtered_df = subdf_channel[(subdf_channel['datetime'].dt.date >= start_time) & 
+                                (subdf_channel['datetime'].dt.date <= end_time)]
 
+    # Преобразуем строку в datetime
+    #subdf_channel.loc[:, 'datetime'] = pd.to_datetime(subdf_channel['datetime'])
+    #start_time = subdf_channel['datetime'].min() + pd.Timedelta(seconds=slider_range[0])
+    #end_time = subdf_channel['datetime'].min() + pd.Timedelta(seconds=slider_range[1])
+
+    #filtered_df = subdf_channel[(subdf_channel['datetime'] >= start_time) & (subdf_channel['datetime'] <= end_time)]
+
+
+    
     filtered_df_uniq = filtered_df[['date', 'day_change_pos', 'day_change_neg']].drop_duplicates()
 
     fig = go.Figure()
-    fig.add_trace(go.Bar(x=filtered_df_uniq['date'], y=filtered_df_uniq['day_change_pos'], marker_color='#F5DEB3', hovertemplate='%{x} <br>Подписались: %{y} <extra></extra>'))
-    fig.add_trace(go.Bar(x=filtered_df_uniq['date'], y=filtered_df_uniq['day_change_neg'], marker_color='#8B0000', hovertemplate='%{x} <br>Отписались: %{y}<extra></extra>'))
+    fig.add_trace(go.Bar(x=filtered_df_uniq['date'], y=filtered_df_uniq['day_change_pos'], 
+                         marker_color='#F5DEB3', 
+                         hovertemplate='%{x} <br>Подписались: %{y} <extra></extra>'))
+    fig.add_trace(go.Bar(x=filtered_df_uniq['date'], y=filtered_df_uniq['day_change_neg'], 
+                         marker_color='#8B0000', 
+                         hovertemplate='%{x} <br>Отписались: %{y}<extra></extra>'))
 
     fig.update_layout(
         showlegend=False,
-        paper_bgcolor= '#ffb347', #'#FFFFFF',
-        plot_bgcolor=  '#ffb347', #'#FFFFFF',
+        paper_bgcolor= '#ffb347',
+        plot_bgcolor=  '#ffb347',
         font_family='Georgia',
         title_font_size=24,
         title_x=0.5,
@@ -41,57 +53,43 @@ def create_subs_pos_neg(subs, channel, slider_range): #, slider_range
         xaxis_title="Дата и время",
         title = '',
         xaxis=dict(
-            rangeselector=dict(  # Добавляем элементы управления диапазоном
-                bgcolor= '#f5dfbf' ,  # Фоновый цвет области с кнопками
-                font=dict(color="#333"),  # Цвет текста на кнопках
-                activecolor= '#ffb347',  # Цвет активной кнопки
-                bordercolor='#f5dfbf',  # Цвет рамки вокруг кнопок                
+            rangeselector=dict(
+                bgcolor= '#f5dfbf',
+                font=dict(color="#333"),
+                activecolor= '#ffb347',
+                bordercolor='#f5dfbf',                
                 buttons=list([
                     dict(count=3, label="3д", step="day", stepmode="backward"),
                     dict(count=7, label="1н", step="day", stepmode="backward"),
                     dict(count=1, label="1м", step="month", stepmode="backward"),
-                    dict(step="all")  # Кнопка для просмотра всего диапазона
+                    dict(step="all")
                 ])
-            )) 
+            ))
     )
     return fig
 
 
+
 def create_slider(subs, channel):
     if channel is None:
-        st.write({})  # Вывод пустой фигуры
-        return
+        return None
 
     subs = subs[subs['channel_name'] == channel]    
-     # Получаем минимальную и максимальную дату
-    date_min = pd.to_datetime(subs['datetime']).min()
-    date_max = pd.to_datetime(subs['datetime']).max()
-    
-    # Создаем интервал между минимальной и максимальной датой
-    time_delta = (date_max - date_min).total_seconds()
-    
-    # Разделим временной промежуток на шаги для слайдера
-    step = 86400 # секунд в одни сутки
-    
-    # Определяем начальные значения слайдера
-    min_value = int(0)
-    max_value = int(time_delta)
-    initial_value = [min_value, max_value]
-    
-    # Конвертируем минимальную и максимальную дату в секунды от начала эпохи (1970-01-01)
-    #min_value = int(date_min.timestamp())
-    #max_value = int(date_max.timestamp())
-    # Начальное значение также должно быть в секундах
-    #initial_value = [min_value, max_value]
+    # Получаем минимальную и максимальную дату
+    date_min = pd.to_datetime(subs['datetime']).min().date()
+    date_max = pd.to_datetime(subs['datetime']).max().date()
     
     # Отображаем слайдер
-    return st.slider(
-                        'Выберите диапазон дат:',
-                        min_value=min_value,
-                        max_value=max_value,
-                        value=initial_value,
-                        step=step,
-                    )   
+    selected_range = st.slider(
+        'Выберите диапазон дат:',
+        min_value=date_min,
+        max_value=date_max,
+        value=(date_min, date_max),
+        format="YYYY-MM-DD"
+    )
+    
+    return selected_range
+
 
 
 
