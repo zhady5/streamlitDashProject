668eecfa166f79d2a331b07148415ea941e92c18
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-03 01:04:38 +0500

    Update fig_image.py

--- fig_image.py
+++ fig_image.py
@@ -5,15 +5,6 @@ from io import BytesIO
 import string
 import pandas as pd
 from collections import Counter
-import nltk 
-# Загрузка необходимых ресурсов NLTK
-
-nltk.download('stopwords')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('brown')
-
-from nltk.corpus import stopwords, brown
-from nltk.tokenize import word_tokenize
 
 def load_stopwords_from_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -29,10 +20,21 @@ exclude = set(['например', 'какие', 'кто-то', 'что-то', '
 numbers = set('1234567890')
 dell_words = stopwords_ru | predlogi | souzy | numbers | exclude
 
+
+# Функция для очистки текста
+def clean_text(text):
+    text = text.lower()  # Приводим весь текст к нижнему регистру
+    text = re.sub(r'[^\w\s]', '', text)  # Удаляем все символы, кроме букв и пробелов
+    words = text.split()  # Разбиваем текст на слова
+    #stop_words = {'и', 'в', 'во', 'не', 'что', 'он', 'она', 'оно', 'они', 'но', 'а', 'это'}  # Простые стоп-слова
+    words = [word for word in words if word not in dell_words]  # Удаляем стоп-слова
+    return words
+    
+
 @st.cache
 def prepare_data(posts, channel):
     posts_channel = posts[posts['channel_name'] == channel]
-    words = posts_channel.text.apply(lambda t: list(set([w.lower() for w in nltk.word_tokenize(t)]) - puncts - dell_words)).tolist()
+    words = posts_channel.text.apply(lambda t: clean_text(t)).tolist()
     df_words = pd.DataFrame(Counter(sum(words, [])).most_common(50), columns=['word', 'count'])
     return df_words
 
