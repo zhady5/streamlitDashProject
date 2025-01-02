453e91f86722bd7a98c88352f95d1ac4f36fe17f
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-02 17:56:02 +0500

    Update fig_subs_pos_neg.py

--- fig_subs_pos_neg.py
+++ fig_subs_pos_neg.py
@@ -85,7 +85,7 @@ def create_slider(subs, channel):
         min_value=date_min,
         max_value=date_max,
         value=(date_min, date_max),
-        format= "Y-B-D"
+        format= "YYYY-MMM-DD"
     )
     
     return selected_range
