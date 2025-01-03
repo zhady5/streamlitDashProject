e9cfd1030ec40579b4702cc27154da018956b92e
Mika <42176090+zhady5@users.noreply.github.com>
2025-01-04 02:00:48 +0500

    Update fig_heatmap.py

--- fig_heatmap.py
+++ fig_heatmap.py
@@ -40,7 +40,7 @@ def create_heatmap(filtered_df):
                 z=z_values,
                 x=x_labels,
                 y=y_labels,
-                colorscale= [[0,  '#F5DEB3'], [[0,0000000001, '#8B0000'], [1, '#8B0000']]], #[[0, '#F5DEB3'], [1, "#006a4e"]],
+                colorscale= [[0,  '#F5DEB3'], [[0.0000000001, '#8B0000'], [1, '#8B0000']]], #[[0, '#F5DEB3'], [1, "#006a4e"]],
                 showscale=False,
                 xgap=10,
                 ygap=10,
